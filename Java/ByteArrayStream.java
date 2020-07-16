699e0370b1c54f8bff8a1be89cd30d777b2c477c
LawranceMichaelite <jeyalawrance@gmail.com>
2020-07-16 18:38:23 +0530

    ByteArrayStream example

--- Java/ByteArrayStream.java
+++ Java/ByteArrayStream.java
@@ -1,1 +1,31 @@
-// TO-DO
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+public class ByteArrayStream{
+
+	public static void main(String[] args) {
+		String value = "This is a Very Good Example";
+		int a = 0;
+		byte[] buf = new byte[value.length()];
+        for(char i : value.toCharArray()) {
+        	buf[a++] = (byte) i;
+        }
+		try(ByteArrayInputStream stream = new ByteArrayInputStream(buf)) {
+			int printVal = stream.read();
+			while(printVal != -1) {
+				System.out.print((char) printVal);
+				printVal = stream.read();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try(ByteArrayOutputStream stream = new ByteArrayOutputStream(); 
+			OutputStream out = new FileOutputStream("./test.txt") ) {
+			stream.write(buf);
+			stream.writeTo(out);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}				
