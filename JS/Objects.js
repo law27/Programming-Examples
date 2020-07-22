2f0be62d2552bf8ed53155dad7691df83a3dc3d1
LawranceMichaelite <jeyalawrance@gmail.com>
2020-07-22 15:53:38 +0530

    JS Object

--- JS/Objects.js
+++ JS/Objects.js
@@ -11,8 +11,60 @@ let user = {
     age: 20,
     id: "igfuh4hj"
 };
-
+/*
 for(let key in user) {
     alert(key);
     alert(user[key]);
 }
+*/
+function problem1() {
+let isEmpty = (schedule) => {
+    for(let key in schedule) {
+        return false;
+    } return true;
+};
+
+
+let schedule = {};
+alert(isEmpty(schedule))
+schedule["Lawrance"] = 2.5;
+alert(isEmpty(schedule))
+}
+
+
+function problem2() {
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = 0;
+
+for(let keys in salaries) {
+    sum += salaries[keys];
+}
+
+    console.log(sum);
+}
+
+function problem3() {
+    let multiplyNumeric = menu => {
+        for(let keys in menu) {
+            if(typeof(menu[keys]) === "number") {
+                menu[keys] *= 2;
+            }
+        }
+    }
+
+    let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+    };
+
+    multiplyNumeric(menu);
+    console.log(menu)
+}
+
+problem3();
