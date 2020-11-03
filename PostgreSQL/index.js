d417b910f04c1275b42d5d3d6e15fd487ee4d57e
law27 <jeyalawrance@gmail.com>
2020-11-03 10:01:36 +0530

    Bruh

--- PostgreSQL/index.js
+++ PostgreSQL/index.js
@@ -2,6 +2,16 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const formatter = require('pg-format')
 
+/*
+    %% outputs a literal % character.
+    %I outputs an escaped SQL identifier.
+    %L outputs an escaped SQL literal.
+    %s outputs a simple string.
+*/
+
+const string = formatter('select * from %I', 'users');
+console.log(string);
+
 
 const pool = new Pool({
   user: process.env.DBUSER,
@@ -10,7 +20,4 @@ const pool = new Pool({
   password: process.env.DBPASSWORD,
   port: process.env.DBPORT
 })
-
-pool.query("SELECT * FROM users", (err, res) => {
-    console.log(res);
-})
+pool.end();
